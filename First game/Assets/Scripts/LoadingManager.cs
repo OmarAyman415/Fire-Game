e5e0ffef85257a41f65917b522e10b3710598e10
OmarAyman415 <o.ayman415@gmail.com>
2022-04-30 08:39:28 +0200

    Polished Level 2

--- First game/Assets/Scripts/LoadingManager.cs
+++ First game/Assets/Scripts/LoadingManager.cs
@@ -4,8 +4,13 @@ using UnityEngine.SceneManagement;
 
 public class LoadingManager : MonoBehaviour
 {
+
+    [Header("Scene Number")]
+    [SerializeField] private int sceneNumber;
     [Header("Next Level Sound")]
     [SerializeField] private AudioClip nextLevelSound;
+
+
     private void Update()
     {
         //if (Input.GetKeyDown(KeyCode.F))
@@ -19,9 +24,7 @@ public class LoadingManager : MonoBehaviour
     {
         if (collider.tag == "Player")
         {
-
-            SceneManager.LoadScene(1);
-            Debug.Log(SceneManager.GetActiveScene().name);
+            SceneManager.LoadScene(sceneNumber);
             SoundManager.instance.PlaySound(nextLevelSound);
         }
     }
